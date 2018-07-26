04a9ed461b74c83daf15f956f0c72c80db927a04
huhailinguist <hu.hai@outlook.com>
2018-07-26 17:36:13 -0400

    Update tbProcess.py

--- tbProcess.py
+++ tbProcess.py
@@ -12,217 +12,155 @@ The input we take are trees that look like this:
 import sys, re
 
 class Node:
-    '''
-    A node has
-    1) tag, e.g. NP
-    2) word, e.g. Apple
-    3) children = a list of nodes
-    '''
+    """ terminal and non-terminal nodes """
     def __init__(self, tag, word=None):
-        self.tag = tag
-        self.word = word
+        self.tag  = tag    # NN
+        self.word = word   # apple
         self.children = []
-
-
-class Tree:
-    '''
-    A tree has
-    1) n = number of nodes
-    2) m = number of layers
-    3) root = root node
-    '''
-    def __init__(self, root=None):
-        self.root = root
-        self.n = 0
-        self.m = 0
-    def traverse(self):
-        pass
+        
+    def __repr__(self):
+        return self.tag
+    
+    def __str__(self):
+        return self.__repr__()
 
 class PCFG():
-    def __init__(self):
-        self.pcfg = {}
-        self.grm_rule = {}
-        self.lex_rule = {}
-        self.grm_lhss = {} # left hand side symbols for grammar rules
-        # grm_lhss is a dict of dicts:
-        # {'S': {'NP VP':2, 'NP VP NP':3}, 'VP': {} ...}
+    def __init__(self, parseTrees=None, tagToCheck=None):
+        self.parseTrees = parseTrees  # a list of parseTrees in penn treebank format
+        self.grm_rule = {}  # {"S -> NP VP": 1, ...}
+        self.lex_rule = {}  # {"NN -> apple": 1, ...}
+        self.grm_lhss = {}  # {"S": {"NP VP":2, "NP VP NP":3}, "VP": {} ...}
+
+        if self.parseTrees:
+            self.getPCFG(self.parseTrees, tagToCheck)
 
     def addGrmRule(self, rule):
-        self.grm_rule[rule] = self.grm_rule.get(rule, 0)+1
+        self.grm_rule[rule] = self.grm_rule.get(rule, 0) + 1
         lhs = rule.split(' -> ')[0]
         rhs = rule.split(' -> ')[1]
         if lhs in self.grm_lhss:
             self.grm_lhss[lhs][rhs] = self.grm_lhss[lhs].get(rhs, 0) + 1
         else:
-            self.grm_lhss[lhs] = {rhs : 1}
+            self.grm_lhss[lhs] = {rhs: 1}
+
     def addLexRule(self, rule):
-        self.lex_rule[rule] = self.lex_rule.get(rule, 0)+1
+        self.lex_rule[rule] = self.lex_rule.get(rule, 0) + 1
 
-def getPCFG(fn, tagToCheck=None, verbose=False, funcTag=False):
-    # if funcTag: we have NP-PN-SBJ, otherwise we have NP
-    trees = open(fn, encoding='utf8').readlines()
-    trees = [x.rstrip() for x in trees]
-
-    grm_rule = {}
-    lex_rule = {}
-
-    pcfg = PCFG()
-
-    # for testing:
-    # t = '( (IP-HLN (NP-PN-SBJ (NR 中) (NR 美)) (VP (PP-LOC (P 在) (NP-PN (NR 沪))) (VP (VV 签订) (NP-OBJ (NP (NP (ADJP (JJ 高)) (NP (NN 科技))) (NP (NN 合作))) (NP (NN 协议)))))) )'
-
-    for t in trees:
-        # -------------- #
-        # pre-processing
-        # -------------- #
-        t = t.strip()
-        # if there are ( or ) in the sentence, corenlp doesn't change them to LOB, RCB,
-        # so we need to do that
-        t = t.replace('PU (', 'PU LOB').replace('PU )', 'PU RCB')
-
-        # if no ROOT symbol, add ROOT
-        if t.startswith('( ('): # TODO what about other treebanks: or t.startswith('((') or t.startswith('(  ('):
-            # print('No ROOT symbol found! Added "ROOT" as ROOT symbol.')
-            t = t.replace('( (', '(ROOT (')
-
-        if t.startswith('(('): #
-            # print('No ROOT symbol found! Added "ROOT" as ROOT symbol.')
-            t = t.replace('((', '(ROOT (')
+    def getPCFG(self, parseTrees, tagToCheck=None, verbose=False, keepFuncTag=True):
+        """
+        extract a list of CFG rules from parseTrees
+        if keepFuncTag: NP-PN-SBJ, otherwise NP, but output of corenlp does not have FuncTag """
 
         # change ( (FRAG(NR 新华社) to ( (FRAG (NR 新华社) Note the space before '('
         pat = re.compile('(\((?P<tag>[A-z|\-]*?)\()')
-        m = pat.findall(t)
-        if m:
-            # print('\nfound (TAG(TAG! lacking a space!')
-            # print([x[0] for x in m])
-            # print('BEFORE:', t)
-            t = pat.sub(r'(\g<tag> (', t)
-            # print('AFTER :', t)
 
-        # index of ( or )
-        indBrc = [pos for pos, char in enumerate(t) if char == '(' or char == ')']
-        # print(indBrc)
+        # for testing:
+        # t = '( (IP-HLN (NP-PN-SBJ (NR 中) (NR 美)) (VP (PP-LOC (P 在) (NP-PN (NR 沪))) (VP (VV 签订) (NP-OBJ (NP (NP (ADJP (JJ 高)) (NP (NN 科技))) (NP (NN 合作))) (NP (NN 协议)))))) )'
 
-        counter_ntn = 0 # non-terminal node, which includes phrasal nodes AND pos tags!
-        stack = []
-        tree = Tree()
-        i = 0
+        for t in parseTrees:
+            ###  pre-processing  ###
+            t = t.strip()
+            # if there are ( or ) in the sentence, corenlp doesn't change them to LOB, RCB,
+            # so we need to do that: LOB=left opening bracket, RCB=right closing bracket
+            t = t.replace('PU (', 'PU LOB').replace('PU )', 'PU RCB')
+            # if no ROOT symbol, add ROOT
+            if t.startswith('( ('):
+                t = t.replace('( (', '(ROOT (')
+            if t.startswith('(('):
+                t = t.replace('((', '(ROOT (')
+            m = pat.findall(t)
+            if m:
+                t = pat.sub(r'(\g<tag> (', t)
 
-        while i < len(indBrc): # i is the index in indBrc[]
-            if t[indBrc[i]] == '(': # and t[indBrc[indOfBrc+1]] == '(':
-                if t[ indBrc[i+1] ] == ')':
-                    # ---------------------------- #
+            # index of ( or )
+            indBrc = [pos for pos, char in enumerate(t) if char == '(' or char == ')']
+
+            stack = []
+            counter_ntn = 0
+            i = 0
+
+            ###  real work  ###
+            while i < len(indBrc):  # i is the index in indBrc[]
+                if t[indBrc[i]] == '(':
+                    # if current Brc is ( and next is )
                     # terminal node, lexical rules
-                    # ---------------------------- #
-                    counter_ntn += 1
+                    if t[indBrc[i + 1]] == ')':
+                        tagNword = t[ (indBrc[i] + 1): indBrc[i + 1] ].split(' ')  # ['NR', '中']
+                        termNode = Node(tagNword[0], tagNword[1])
+                        lex_rule = tagNword[0] + ' -> ' + tagNword[1]
+                        self.addLexRule(lex_rule)
 
-                    tmp = t[(indBrc[i] + 1) : indBrc[i + 1]] # NR 中
-                    tmp = tmp.split(' ')
-                    node_tmp = Node(tmp[0], tmp[1]) # NR, 中
-                    # print('terminal node:', tmp, 'No.{}'.format(counter_ntn))
-                    rule = tmp[0] + ' -> ' + tmp[1]
-                    # ----------------------------------------
-                    lex_rule[rule] = lex_rule.get(rule, 0) + 1
-                    pcfg.addLexRule(rule)
-                    # ----------------------------------------
+                        # add to the children of first node in stack
+                        stack[-1].children.append(termNode)
 
-                    # TODO will this happen? probably not
-                    if counter_ntn == 1: # root
-                        tree.root = node_tmp
+                        i += 2  # skip the ')' in terminal node (NR 中)
 
-                    # add to the children of first node in stack
-                    stack[-1].children.append(node_tmp)
-
-                    i += 2 # skip the ')' in terminal node (NR 中)
-                    continue # this is used to skip: if t[indBrc[i]] == ')': ?? Maybe not
-
-                else:
-                    # ---------------------------- #
+                    # current Brc is (, next is also (
                     # a phrasal node, grammar rule
-                    # ---------------------------- #
-                    counter_ntn += 1
-                    tag = t[ (indBrc[i] + 1) : indBrc[i+1] ]
-                    tag = tag.rstrip() # get rid of the \s in NP\s
-
-                    if not funcTag: # change NP-PN-SBJ to just NP
-                        if '-' in tag:
-                            idx_hyphen = tag.find('-')
-                            if idx_hyphen >= 2:
-                                tag = tag[:idx_hyphen]
-                            else:
-                                print('tag is:', tag)
-                                exit('something wrong when removing functional tags')
-                    node_tmp = Node(tag)
-                    # print('phrasal node:', tag, 'No.{}'.format(counter_ntn))
-
-                    if counter_ntn == 1: # root
-                        tree.root = node_tmp
                     else:
-                        # if not root, append to child
-                        try:
-                            stack[-1].children.append(node_tmp)
-                        except IndexError: 
-                            # TODO
-                            # there will be indexerror if in the input file, 
-                            # there are 2 trees in one line.
-                            print('\n!! two trees in one line:')
-                            print(t)
-                            break
+                        tag = t[(indBrc[i] + 1): indBrc[i + 1]].rstrip()
 
-                    # add to stack
-                    stack.append(node_tmp)
+                        if not keepFuncTag:  # change NP-PN-SBJ to just NP
+                            if '-' in tag:
+                                idx_hyphen = tag.find('-')
+                                if idx_hyphen >= 2:
+                                    tag = tag[:idx_hyphen]
+                                else:
+                                    print('tag is:', tag)
+                                    exit()
+                        nonTermNode = Node(tag)
+                        
+                        counter_ntn += 1
+                        if counter_ntn == 1:  # root
+                            pass
+                        else:
+                            # if not root, append to child of most recent node
+                            stack[-1].children.append(nonTermNode)
+                            
+                        # add to stack
+                        stack.append(nonTermNode)
+                        i += 1
 
-            if t[indBrc[i]] == ')': # pop a node from the stack
-                lastNode = stack.pop(-1)
-                LHS = lastNode.tag
-                RHS = ' '.join([x.tag for x in lastNode.children])
-                rule = LHS + ' -> ' + RHS
-                # if rule == 'IP -> -NONE-':
-                #     print(t)
-                # print(rule)
-                # ----------------------------------------
-                grm_rule[rule] = grm_rule.get(rule, 0) + 1
-                pcfg.addGrmRule(rule)
-                # ----------------------------------------
+                # current node is )
+                elif t[indBrc[i]] == ')':  # pop a node from the stack
+                    lastNode = stack.pop(-1)
+                    LHS = lastNode.tag
+                    RHS = ' '.join([x.tag for x in lastNode.children])
+                    grmrule = LHS + ' -> ' + RHS
+                    self.addGrmRule(grmrule)
+                    i += 1
 
-            i+=1
+            # make sure nothing is left in stack
+            assert len(stack) == 0
 
-        # make sure nothing is left in stack
-        assert len(stack) == 0
+        if verbose:
+            print('\nlex_rule:')
+            for k, v in sorted(self.lex_rule.items(), key=lambda x: x[1]):  # key is rule, value is count
+                print('{:>10}\t{}'.format(v, k))
 
-    print()
+            print('\ngrm_rule:')
+            for k, v in sorted(self.grm_rule.items(), key=lambda x: x[1]):
+                print('{:>10}\t{}'.format(v, k))
 
-    if verbose:
-        print('\nlex_rule:')
-        for k, v in sorted(lex_rule.items(), key=lambda x: x[1]): # key is rule, value is count
-            print('{:>10}\t{}'.format(v, k))
-        
-        print('\ngrm_rule:')
-        for k, v in sorted(grm_rule.items(), key=lambda x: x[1]):
-            print('{:>10}\t{}'.format(v, k))
+            print('num of lex rules:', len(self.lex_rule))
+            print('num of grm rules:', len(self.grm_rule))
 
+        if tagToCheck is not None:
+            print("pcfg.grm_lhss[{}]".format(tagToCheck))
+            for k, v in sorted(self.grm_lhss[tagToCheck].items(), key=lambda x: x[1]):
+                print('{:>10}\t{}'.format(v, k))
+            print('\n# of rule types :', len(self.grm_lhss[tagToCheck]))
+            print('# of rule counts:', sum(self.grm_lhss[tagToCheck].values()))
 
-    print()
-    print('num of lex rules:', len(lex_rule))
-    print('num of grm rules:', len(grm_rule))
-    print()
-
-    if tagToCheck is not None:
-        print("pcfg.grm_lhss[{}]".format(tagToCheck))
-        for k, v in sorted(pcfg.grm_lhss[tagToCheck].items(), key=lambda x: x[1]):
-            print('{:>10}\t{}'.format(v, k))
-        # print('\n'.join(pcfg.grm_lhss.keys()))
-        print('\n# of rule types :', len(pcfg.grm_lhss[tagToCheck]))
-        print('# of rule counts:', sum(pcfg.grm_lhss[tagToCheck].values()))
-
+def main(args):
+    trees = open(args[1]).readlines()
+    if len(args) == 2:
+        pcfg = PCFG(trees)
+    else:
+        pcfg = PCFG(trees, args[2])
+            
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage: python tbProcess.py filename (tag)')
-        # getPCFG('brc_split2_test', False)
-        # getPCFG('oneSent', False)
-    elif len(sys.argv) == 2:
-        getPCFG(sys.argv[1], verbose=True)
-    elif len(sys.argv) == 3:
-        getPCFG(sys.argv[1], sys.argv[2])
-
-
+    else:
+        main(sys.argv)
